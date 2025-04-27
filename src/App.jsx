7f9bd4e3e42f385f94c8104d7afadfb6fe55fd6b
makeprodigy <payneparihar13@gmail.com>
2025-04-27 17:29:07 +0530

    intialized the loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Navbar from "./components/common/Navbar";
 import Footer from "./components/common/Footer";
+import LoadingScreen from './components/loadingscreen/LoadingScreen';
+
+import CartCheckoutPage from './pages/CartCheckoutPage';
+import HomePage from './pages/HomePage';
+import LoginSignupPage from './pages/LoginSignupPage';
+import ProductDetailsPage from './pages/ProductDetailsPage';
+import ProductsPage from './pages/ProductsPage';
+import SellerDashboardPage from './pages/SellerDashboardPage';
+import UserProfilePage from './pages/UserProfilePage';
+
+import "./App.css";
 
 const App = () => {
+  const [pageLoading, setPageLoading] = useState(true);
+
+  useEffect(() => {
+    const loadingTimer = setTimeout(() => {
+      setPageLoading(false);
+    },3000);
+
+    return () => clearTimeout(loadingTimer);
+  })
+
+  if (pageLoading) {
+    return <LoadingScreen />
+  }
+
   return (
     <>
 
@@ -11,4 +36,4 @@ const App = () => {
   )
 }
 
-export default App
+export default App;
