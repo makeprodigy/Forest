79345f778c3222437b2d4fd7f7277215e9874e1b
makeprodigy <payneparihar13@gmail.com>
2025-06-14 10:42:45 +0530

    fixed jsx in sellerdashboardpage

--- src/pages/SellerDashboardPage.jsx
+++ src/pages/SellerDashboardPage.jsx
@@ -1,32 +1,17 @@
 import React from 'react';
+import '../pagescss/SellerDashboardPage.css';
 import SellerStats from '../components/sellerdashboard/SellerStats';
+import OrderHistory from '../components/sellerdashboard/OrderHistory';
 import AddProductForm from '../components/sellerdashboard/AddProductForm';
 import ProductManagementPanel from '../components/sellerdashboard/ProductManagementPanel';
-import OrderHistory from '../components/sellerdashboard/OrderHistory';
-
-import '../pagescss/SellerDashboardPage.css';
 
 const SellerDashboardPage = () => {
   return (
-    <div className="seller-dashboard">
-
-      <section className="seller-summary">
-        <h2 className='seller-greeting'>Welcome Back, Seller</h2>
-        <SellerStats />
-      </section>
-
-      <section className="add-product-section">
-        <AddProductForm />
-      </section>
-
-      <section className="product-management-section">
-        <ProductManagementPanel />
-      </section>
-
-      <section className="order-history-section">
-        <OrderHistory />
-      </section>
-
+    <div className="seller-dashboard-grid">
+      <SellerStats />
+      <OrderHistory />
+      <AddProductForm />
+      <ProductManagementPanel />
     </div>
   );
 };
